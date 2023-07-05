dff9357d6a78ac4babe2656ba54339ab9b940622
Jeff <kbj@zigbang.com>
2023-07-06 00:20:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 객체지향
+### 객체지향의 정의
+- 현실 세계에서 개체(Entity)를 속성(Attribute 또는 Property) 과 메소드 (Method)를 결합한 형태의 객체(Object) 로 표현하는 개념
+- 구현해야 하는 대상을 하나의 객체로 보고 그 객체와 다른 객체들간의 관계로서 S/W를 모델링하는 개발방법론
+ 
+- ![pic](images/object-oriented-concep.png)
+
---
+++ encapsulation.ts
@@ -0,0 +1,16 @@
+export class Time {
+	private hour: number
+	private minute: number
+	private second: number
+
+	constructor(hour: number, minute: number, second: number) {
+		this.hour = hour
+		this.minute = minute
+		this.second = second
+	}
+}
+
+
+const time = new Time(12, 15, 0)
+console.log(time)
+console.log(time.hour)
